47273be014db1cfd765e974a3b6bb9b701e2c44f
hoddi84 <hoddipsx@hotmail.co.uk>
2016-02-18 17:50:13 +0000

    Minimax

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,36 +1,45 @@
 import java.util.ArrayList;
 
 public class MiniMax {
-	public ArrayList<Move> actions = new ArrayList<Move>();
-	public boolean isDraw = false;
-	int alpha, beta;
-	int score;
-	int level;
-	public MiniMax(int level, boolean player, int alpha, int beta){
-	/*
-		actions = getLegalActions();
-		if (actions == null)
-			{
-				isDraw = true;	
-			}
-		if(level == 0)
+	//public ArrayList<Move> actions = new ArrayList<Move>();
+	public static int score;
+	public static int MiniMax(State state, int depth, boolean isMyTurn, int alpha, int beta){
+		
+		ArrayList<Move> actions = new ArrayList<Move>();
+		//TODO I think we are calling legalmoves 2x check it out.
+		if(depth == 0 || state.getUtility(isMyTurn) != -1)
 		{
-			return score;	
+			return state.evaluate();	
 		}
 		
-		if(player)//max
-			for(Move action : actions)
-				score = MiniMax(level -1, !player, alpha, beta);
-				if (score > alpha) alpha = score;
-				if (alpha >= beta) break;  // beta cut-off
-				return alpha;
+		actions = state.getLegalMoves(isMyTurn);
+
+		if (isMyTurn){
+			score = Integer.MIN_VALUE;
+			for (Move action : actions)
+			{	//bs state stuff need to fix
+				State newState = state.getNextState(action);
+				score = Math.max(score,MiniMax(newState, depth -1, !isMyTurn, alpha, beta));
+				alpha = Math.max(alpha, score);
+				if (beta <= alpha) break; // beta cut-off
+				return score;	
+			}
+		}
+		else{
+			score = Integer.MAX_VALUE;
+			for (Move action : actions)
+			{	//bs state stuff need to fix
+				State newState = state.getNextState(action);
+				score = Math.min(score, MiniMax(newState, depth -1, !isMyTurn, alpha, beta));
+				beta = Math.min(beta, score);
+				if (beta <= alpha) break; // alpha cut-off
+				return score;
+				
+			}			
+		}
+		//originally to get rid of error, maybe find a way to remove.
+		return state.evaluate();
 		
-		else // min
-			for each action
-				score = minimax(level -1, !player, alpha, beta);
-				if (score < beta) beta = score;
-				if (alpha >= beta) break;  // alpha cut-off
-			return beta;
-			*/
+				
 	}
 }
