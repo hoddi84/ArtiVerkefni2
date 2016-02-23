0784ca8581d064de276955e90609a8be2502df6f
hoddi84 <hoddipsx@hotmail.co.uk>
2016-02-23 18:21:06 +0000

    Added role to try to fix for two players.

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,13 @@ public class Main {
 		try{
 			// TODO: put in your agent here
 			Agent agent = new AlphaBetaAgent();
-			Agent agent2 = new AlphaBetaAgent();
 			
-			int port = 4001, port2 = 4002;
+			int port = 4001;
 			if (args.length>=1){
 				port=Integer.parseInt(args[0]);
 			}
-			if (args.length>=2){
-				port2=Integer.parseInt(args[1]);
-			}
 			GamePlayer gp=new GamePlayer(port, agent);
-			GamePlayer gp2=new GamePlayer(port2, agent2);
 			gp.waitForExit();
-			gp2.waitForExit();
 		}catch(Exception ex){
 			ex.printStackTrace();
 			System.exit(-1);
